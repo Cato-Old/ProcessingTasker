a3b858e4c0e409468956b43e69454928e564b173
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-01-12 22:18:02 +0100

    Adding displaying of the publication's current processing status.

--- src/main/java/Model/Tasks/CopyAndRenumberTask.java
+++ src/main/java/Model/Tasks/CopyAndRenumberTask.java
@@ -1,1 +1,33 @@
-package Model.Tasks;import javafx.application.Platform;import javafx.concurrent.Task;import javafx.scene.control.TextArea;import java.io.BufferedReader;import java.io.InputStreamReader;public class CopyAndRenumberTask extends ProcessTask {    public CopyAndRenumberTask(TextArea consoleTextArea) {        super(consoleTextArea);    }    @Override    public Task task() {        return new Task() {            @Override            protected Void call() throws Exception {                String line;                Runtime rt = Runtime.getRuntime();                Process proc = rt.exec("python C:\\Java\\projects\\ProcessingTasker\\script.py");                try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {                    while((line = reader.readLine()) != null){                        final String ln = "\n" + line;                        Platform.runLater(()->consoleTextArea.appendText(ln));                    }                } catch (Exception e){e.printStackTrace();}                return null;            }        };    }}
+package Model.Tasks;
+
+import Model.Projects.Publications.Publication;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CopyAndRenumberTask extends ProcessTask {
+
+    public CopyAndRenumberTask(Publication publication) {
+        super(publication);
+    }
+
+    @Override
+    public Task task() {
+        return new Task() {
+            @Override
+            protected Void call() throws Exception {
+                String line;
+                Runtime rt = Runtime.getRuntime();
+                Process proc = rt.exec("python C:\\Java\\projects\\ProcessingTasker\\script.py");
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
+                    while((line = reader.readLine()) != null){
+                        final String ln = "\n" + line;
+                        Platform.runLater(()->consoleTextArea.appendText(ln));
+                    }
+                } catch (Exception e){e.printStackTrace();}
+                return null;
+            }
+        };
+    }
+}
